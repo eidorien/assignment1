c0b3200dfd39e6b8eb444adb0338c601724ba906
eidorien <68266570+eidorien@users.noreply.github.com>
2020-07-13 22:52:40 -0700

    Initial commit

---
+++ store.js
@@ -0,0 +1,5 @@
+var apples = 5
+var peaches = 7
+var totalFruitCount = apples + peaches;
+
+document.write('<p> Welcome to my calculator</p>' + '<p>The sum of ' + apples + ' + ' + peaches + ' is : ' + totalFruitCount + '</p>');
